2acee0c0c6330760975b81323379c9febd42eafa
royr40122-oss <royr40122@gmail.com>
2026-02-28 11:48:05 +0530

    Update API credentials in vars.py

--- vars.py
+++ vars.py
@@ -3,6 +3,6 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "2447366")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "5dghgfccc") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "7231089830:A") #Replace with your bot token
+API_ID = int(environ.get("API_ID", "31445635")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "15c057ca3aa85360ac3d63de52368718") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "7952763523:AAEDuVFshqXJL2vIicFbMYwqQzX12-TkdtE") #Replace with your bot token
